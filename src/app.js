32eba01568549db44a511df0372905bcf5fbee91
syhaminreal <shyaminreallife@gmail.com>
2026-04-13 16:56:45 +0545

    loggresdone till here

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from 'express';
-
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from  'cookie-parser';
 
 const app = express();
 
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(cookieParser());
+
+app.use(morgan('dev'));
+
+
+
 app.get('/', (req, res) =>{
+  logger.info('Hello from Accuistion');
+
   res.status(200).send('Hello from Acquisitions');
 });
 
