badee9120f5808f3be4214b1b3541ca89bd68dfe
syhaminreal <shyaminreallife@gmail.com>
2026-04-13 16:04:06 +0545

    drizzle set up for now

---
+++ drizzle/0000_calm_warbound.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
---
+++ src/drizzle.config.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+
+export default {
+  schema:'./src/model/*.js',
+  out:'./drizzle',
+  dialect:'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  }
+};
